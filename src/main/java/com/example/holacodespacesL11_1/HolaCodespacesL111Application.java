6c072a93deb151802491a7a0bcb7cb9dfce7d292
pt74847781 <134320456+pt74847781@users.noreply.github.com>
2023-06-26 19:49:29 +0000

    commitprincipal

---
+++ src/main/java/com/example/holacodespacesL11_1/HolaCodespacesL111Application.java
@@ -0,0 +1,13 @@
+package com.example.holacodespacesL11_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolaCodespacesL111Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolaCodespacesL111Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/holacodespacesL11_1/HolaCodespacesL111ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.holacodespacesL11_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolaCodespacesL111ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
